ef4ee7deb32db4d0d1c426d786fc93b71beab861
jeongas9810@gmail.com <jeongas9810@gmail.com>
2022-08-06 14:41:36 +0900

    :card_file_box: 데이터베이스 구조 수정

--- src/main/kotlin/com/smu/som/domain/question/dto/CreateQuestionDTO.kt
+++ src/main/kotlin/com/smu/som/domain/question/dto/CreateQuestionDTO.kt
@@ -4,14 +4,12 @@ import com.smu.som.domain.question.entities.Question
 import com.smu.som.domain.question.entities.Target;
 
 class CreateQuestionDTO(
-	val target: Target,
-	val category: String,
-	val question: String
+	var target: Target,
+	var question: String
 ) {
 	fun toEntity(): Question {
 		return Question(
 			target = target,
-			category = category,
 			question = question
 		)
 	}
--- src/main/kotlin/com/smu/som/domain/question/dto/ReadQuestionDTO.kt
+++ src/main/kotlin/com/smu/som/domain/question/dto/ReadQuestionDTO.kt
@@ -3,8 +3,7 @@ package com.smu.som.domain.question.dto
 import com.smu.som.domain.question.entities.Target;
 
 class ReadQuestionDTO(
-	val id: Long? = null,
-	val target: Target,
-	val category: String,
-	val question: String
+	var id: Long,
+	var target: Target,
+	var question: String
 )
