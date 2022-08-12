d856c9124db2a8da6cf416d768f192ecb1b6d883
limjimin <dkdldsla0000@naver.com>
2022-08-12 10:15:56 +0900

    :wrench: 빈 등록

---
+++ src/main/kotlin/com/smu/som/common/config/RestTemplateConfig.kt
@@ -0,0 +1,13 @@
+package com.smu.som.common.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
+
+@Configuration
+class RestTemplateConfig {
+	@Bean
+	fun restTemplate(): RestTemplate {
+		return RestTemplate()
+	}
+}
