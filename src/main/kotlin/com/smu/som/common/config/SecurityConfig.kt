cd72875b0784f19c347eadb4eef6f71fb2913c45
limjimin <dkdldsla0000@naver.com>
2022-08-13 21:54:43 +0900

    :recycle: filter bean 등록

--- src/main/kotlin/com/smu/som/common/config/SecurityConfig.kt
+++ src/main/kotlin/com/smu/som/common/config/SecurityConfig.kt
@@ -2,32 +2,40 @@ package com.smu.som.common.config
 
 import com.smu.som.common.jwt.filter.JwtAuthenticationFilter
 import com.smu.som.common.jwt.util.JwtResolver
+import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @EnableWebSecurity
 class SecurityConfig(
-	private val jwtResolver: JwtResolver
-) : WebSecurityConfigurerAdapter() {
-	override fun configure(web: WebSecurity) {
-		web
-			.ignoring().antMatchers("/api/auth/**")
+	private val jwtResolver: JwtResolver)
+{
+	@Bean
+	fun webSecurityCustomizer(): WebSecurityCustomizer? {
+		return WebSecurityCustomizer {
+			web: WebSecurity -> web.ignoring().antMatchers("/api/auth/**")
+		}
 	}
 
-	override fun configure(http: HttpSecurity) {
-		http
+	@Bean
+	fun filterChain(http: HttpSecurity?): SecurityFilterChain {
+		http!!
 			.httpBasic().disable()
 			.csrf().disable()
-			.httpBasic().disable()
+			.exceptionHandling()
+			.and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
 			.authorizeRequests()
 			.antMatchers("/api/auth/**").permitAll()
+			.anyRequest().authenticated()
 			.and()
 			.addFilterBefore(JwtAuthenticationFilter(jwtResolver), UsernamePasswordAuthenticationFilter::class.java)
+		return http.build()
 	}
 }
