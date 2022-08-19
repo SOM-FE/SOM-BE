d0cc955228848b138c34c85a3591994ae5b1590a
limjimin <dkdldsla0000@naver.com>
2022-08-19 15:53:58 +0900

    :adhesive_bandage: user entity 수정

--- src/main/kotlin/com/smu/som/domain/user/entity/User.kt
+++ src/main/kotlin/com/smu/som/domain/user/entity/User.kt
@@ -1,6 +1,7 @@
 package com.smu.som.domain.user.entity
 
 import com.smu.som.domain.user.dto.Oauth2UserDTO
+import java.time.LocalDate
 import javax.persistence.*
 
 @Entity
@@ -17,15 +18,25 @@ class User(
 	@Enumerated(EnumType.STRING)
 	var gender: Gender?,
 
-	var nickname: String
+	var nickname: String,
+
+	var email: String?,
+
+	var maritalStatus: Boolean = false,
+
+	var date: LocalDate? = null
+
 ) {
 	companion object {
-		fun of(oAuth2User: Oauth2UserDTO): User {
+		fun of(oAuth2User: Oauth2UserDTO, maritalStatus: Boolean, date: LocalDate?): User {
 			return User(
 				oauth2Id = oAuth2User.oauth2Id,
 				ageRange = oAuth2User.ageRange,
 				gender = Gender.valueOf(oAuth2User.gender),
-				nickname = oAuth2User.nickname
+				nickname = oAuth2User.nickname,
+				email = oAuth2User.email,
+				maritalStatus = maritalStatus,
+				date = date
 			)
 		}
 	}
