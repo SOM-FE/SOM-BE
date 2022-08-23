5ee0b607d7fa6477c3323ea7aa4f1943614bfb4a
jeongas9810@gmail.com <jeongas9810@gmail.com>
2022-08-24 06:48:33 +0900

    :art: DTO Structure 변경

--- src/main/kotlin/com/smu/som/domain/question/dto/ReadQuestionDTO.kt
+++ src/main/kotlin/com/smu/som/domain/question/dto/ReadQuestionDTO.kt
@@ -1,10 +1,22 @@
 package com.smu.som.domain.question.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.smu.som.domain.question.entity.Category
 import com.smu.som.domain.question.entity.Target;
 
 class ReadQuestionDTO(
+	@JsonProperty("id")
 	var id: Long,
+
+	@JsonProperty("target")
 	var target: Target,
+
+	@JsonProperty("question")
 	var question: String,
-	var adult_status: String
+
+	@JsonProperty("isAdult")
+	var isAdult: String,
+
+	@JsonProperty("category")
+	var category: Category
 )
