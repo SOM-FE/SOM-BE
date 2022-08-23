6178f4996672b7a1ab48564e1ea61cdbaa76cabe
jeongas9810@gmail.com <jeongas9810@gmail.com>
2022-08-24 06:49:31 +0900

    :zap: 코드의 성능 개선 및 성인여부 validation 추가

--- src/main/kotlin/com/smu/som/domain/question/repository/QuestionRepository.kt
+++ src/main/kotlin/com/smu/som/domain/question/repository/QuestionRepository.kt
@@ -1,10 +1,11 @@
 package com.smu.som.domain.question.repository
 
+import com.smu.som.domain.question.entity.Category
 import com.smu.som.domain.question.entity.Question
 import com.smu.som.domain.question.entity.Target
 import org.springframework.data.repository.CrudRepository
 
 interface QuestionRepository : CrudRepository<Question, Long> {
-	fun findByTargetOrTarget(target: Target, target2: Target): List<Question>
-	fun findQuestionById(id: Long): String
+	fun findByTargetAndCategoryAndIsAdult(target: Target, category: Category, isAdult: String): List<Question>
+	fun findByTargetAndCategory(target: Target, category: Category): List<Question>
 }
--- src/main/kotlin/com/smu/som/domain/question/service/QuestionService.kt
+++ src/main/kotlin/com/smu/som/domain/question/service/QuestionService.kt
@@ -1,31 +1,22 @@
 package com.smu.som.domain.question.service
 
-import com.smu.som.controller.error.BusinessException
-import com.smu.som.controller.error.ErrorCode
 import com.smu.som.domain.question.dto.CreateQuestionDTO
 import com.smu.som.domain.question.dto.ReadQuestionDTO
+import com.smu.som.domain.question.entity.Category
+import com.smu.som.domain.question.entity.Question
 import com.smu.som.domain.question.entity.Target
 import com.smu.som.domain.question.repository.QuestionRepository
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional(readOnly = true)
 class QuestionService(
 	private val questionRepository: QuestionRepository
 ) {
-	@Transactional(readOnly = true)
 	fun getQuestions(): List<ReadQuestionDTO> {
 		val question = questionRepository.findAll()
 		return question.map { it.toReadQuestionDTO() }
-	}
-
-	@Transactional(readOnly = true)
-	fun getQuestion(id: Long): String {
-		val question = questionRepository.findByIdOrNull(id)
-			?: throw BusinessException(ErrorCode.QUESTION_NOT_FOUND)
-
-		return question.question
 	}
 
 	@Transactional
@@ -34,10 +25,22 @@ class QuestionService(
 		return question.toCreateQuestionDTO()
 	}
 
-	@Transactional(readOnly = true)
-	fun randomQuestion(target: Target): MutableList<Long> {
-		var question = questionRepository.findByTargetOrTarget(target, Target.COMMON)
-		var questionNoList = question.map { it.id }
-		return questionNoList.shuffled().toMutableList()
+	// List<Question>은 non-null 이라 Exception 을 발생시키지 않음 -> throw BusinessException 제거
+	fun randomQuestion(target: Target, category: Category, isAdult: Boolean): List<Question> {
+		println(isAdult)
+		var familyQuestion: List<Question> = emptyList()
+		var commonQuestion: List<Question> = emptyList()
+		var question: List<Question> = if(isAdult) {
+			commonQuestion = questionRepository.findByTargetAndCategory(Target.COMMON, category)
+			questionRepository.findByTargetAndCategory(target, category)
+		} else {
+			commonQuestion = questionRepository.findByTargetAndCategoryAndIsAdult(Target.COMMON, category, "n")
+			questionRepository.findByTargetAndCategoryAndIsAdult(target, category, "n")
+		}
+		if(target == Target.PARENT || target == Target.CHILD) {
+			familyQuestion = questionRepository.findByTargetAndCategory(Target.FAMILY, category)
+		}
+		var questionList: MutableList<Question> = (commonQuestion + question + familyQuestion).map { it.question } as MutableList<Question>
+		return questionList.shuffled()
 	}
 }
