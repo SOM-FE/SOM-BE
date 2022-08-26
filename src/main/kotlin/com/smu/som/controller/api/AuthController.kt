be72fd43e1865d4c9e1d4c8d242419d0b68b0e9f
limjimin <dkdldsla0000@naver.com>
2022-08-26 18:43:45 +0900

    :sparkles: refresh api 작성

--- src/main/kotlin/com/smu/som/controller/api/AuthController.kt
+++ src/main/kotlin/com/smu/som/controller/api/AuthController.kt
@@ -1,5 +1,6 @@
 package com.smu.som.controller.api
 
+import com.smu.som.common.jwt.util.JwtResolver
 import com.smu.som.controller.error.BusinessException
 import com.smu.som.controller.error.ErrorCode
 import com.smu.som.domain.user.dto.JwtTokenDTO
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
 
 
 @RestController
 @RequestMapping("/api/auth")
 class AuthController(
-	private val authService: AuthService
+	private val authService: AuthService,
+	private val jwtResolver: JwtResolver
 ) {
 	@PostMapping("/signup")
 	fun signup(@RequestBody signUpRequestDTO: SignUpRequestDTO): ResponseEntity<SignUpResponseDTO> {
@@ -31,6 +34,14 @@ class AuthController(
 	fun signin(@RequestBody signInRequestDTO: SignInRequestDTO): ResponseEntity<JwtTokenDTO> {
 		return ResponseEntity.ok().body(
 			authService.signin(signInRequestDTO)
+		)
+	}
+
+	@PostMapping("/refresh")
+	fun refresh(request: HttpServletRequest): ResponseEntity<JwtTokenDTO> {
+		val refreshToken = jwtResolver.resolveRefreshToken(request)
+		return ResponseEntity.ok().body(
+			authService.refresh(refreshToken)
 		)
 	}
 
