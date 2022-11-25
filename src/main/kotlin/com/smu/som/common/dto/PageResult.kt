604f5b286cb77b44db43bd79bb1b5c3c4b255685
limjimin <dkdldsla0000@naver.com>
2022-11-25 13:44:07 +0900

    :sparkles: Page 템플릿 작성

---
+++ src/main/kotlin/com/smu/som/common/dto/PageResult.kt
@@ -0,0 +1,27 @@
+package com.smu.som.common.dto
+
+import org.springframework.data.domain.Page
+
+class PageResult<T>(
+	data: Page<T>
+) {
+	val currentPage: Int
+	val totalPage: Int
+	val pageSize: Int
+	val totalElements: Long
+	val content: List<T>
+
+	init {
+		currentPage = data.pageable.pageNumber
+		pageSize = data.pageable.pageSize
+		totalPage = data.totalPages
+		totalElements = data.totalElements
+		content = data.content
+	}
+
+	companion object {
+		fun <T> ok(data: Page<T>): PageResult<T> {
+			return PageResult(data)
+		}
+	}
+}
